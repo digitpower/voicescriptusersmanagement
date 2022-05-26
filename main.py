ba8eade25520355a0f7aa6dc78582f88ae5e6a56
Alexander Gureshidze <alexander@growmo.re>
2022-05-26 16:49:26 +0400

    Added Flask Routes

--- main.py
+++ main.py
@@ -1,25 +1,123 @@
-from jmespath import search
 from licensing.methods import Customer, Key
-import os
 import requests
 import csv
-from readenv import customer_operations_token, key_operations_token
+from readenv import customer_operations_token, key_operations_token, product_id, max_no_of_machines
+from flask import Flask, request
+app = Flask(__name__)
+
+
+features_map = {
+    "f1" : "feature1",
+    "f2" : "feature2",
+    "f3" : "feature3",
+    "f4" : "feature4",
+    "f5" : "feature5",
+    "f6" : "feature6",
+    "f7" : "feature7",
+    "f8" : "feature8"
+}
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
 
 
-def getCustomers(search_data) :
-    
-    search_key = ''
-    if 'email' in search_data :
-        search_key = 'email'
-    if 'name' in search_data :
-        search_key = 'name'
-    if 'company' in search_data :
-        search_key = 'company'
-    
-    print(f"search_data {search_data} search_key {search_key} search_data[search_key] {search_data[search_key]}")
-    
-    PARAMS = {'search' : search_data[search_key], 'token' : customer_operations_token}
+@app.route('/getCustomers', methods = ['GET'])
+def getCustomers():
+    if request.method == 'GET':
+        res = _getCustomers()
+        return {"customers" : res}
+
+        
+@app.route('/getCustomer', methods = ['GET'])
+def getCustomer():
+    if request.method == 'GET':
+        email = request.args.get("email")
+        res = _getCustomer(email)
+        return {"customers" : res}
+
+@app.route('/createCustomer', methods = ['GET'])
+def createCustomer():
+    if request.method == 'GET':
+        customerData = {}
+        customerData["name"] = request.args.get("name") 
+        customerData["email"] = request.args.get("email")
+        customerData["company"] = request.args.get("company")
+        customerData["productId"] = request.args.get("productId")
+        customerData["period"] = request.args.get("period")
+        customerData["f1"] = request.args.get("f1")
+        customerData["f2"] = request.args.get("f2")
+        customerData["f3"] = request.args.get("f3")
+        customerData["f4"] = request.args.get("f4")
+        customerData["f5"] = request.args.get("f5")
+        customerData["f6"] = request.args.get("f6")
+        customerData["f7"] = request.args.get("f7")
+        customerData["f8"] = request.args.get("f8")
+        result = _createCustomer(customerData)
+    return result
+
+@app.route('/deleteUser', methods = ['GET'])
+def deleteCustomer():
+    if request.method == 'GET':
+        res = _deleteCustomer(request.args.get("email"))
+        return res
+
+@app.route('/revokeKey', methods = ['GET'])
+def revokeKey():
+    if request.method == 'GET': 
+        productId = request.args.get("productId")
+        key = request.args.get("key")
+        res = _revokeKey(productId, key)
+        return res
+
+@app.route('/createLicense', methods = ['GET'])
+def createLicense():
+    if request.method == 'GET': 
+        license = {}
+        license["period"] = request.args.get("period")
+        license["f1"] = request.args.get("f1")
+        license["f2"] = request.args.get("f2")
+        license["f3"] = request.args.get("f3")
+        license["f4"] = request.args.get("f4")
+        license["f5"] = request.args.get("f5")
+        license["f6"] = request.args.get("f6")
+        license["f7"] = request.args.get("f7")
+        license["f8"] = request.args.get("f8")
+        res = _createLicense(request.args.get("email"), license)
+        return res
+
+def _getCustomerLicenses(customerId):
+    PARAMS = {'customerId' : customerId, 
+              'token' : customer_operations_token}
+    URL = "https://api.cryptolens.io/api/customer/GetCustomerLicenses"
+    r = requests.get(url = URL, params = PARAMS)
+    json_res = r.json()
+    if json_res["result"] == 0:
+        return (True, json_res["licenseKeys"])
+    else :
+        return (False, [])
+
+def _getCustomers():
+    PARAMS = {'token' : customer_operations_token}
+    URL = 'https://api.cryptolens.io/api/customer/GetCustomers'
+    r = requests.get(url = URL, params = PARAMS)
+    json_res = r.json()
+    if 'customers' not in json_res :
+        return {"result": True, "customers" : []}
+    customers = json_res['customers']
+    for cust in customers:
+        res = _getCustomerLicenses(cust["id"])
+        if(res[0] == True) :
+            licenses = res[1]
+            if len(licenses) == 1 :
+                license = licenses[0]
+                cust["period"],cust["f1"],cust["f2"],cust["f3"],cust["f4"],cust["f5"],cust["f6"],cust["f7"],cust["f8"] = license["period"],license["f1"],license["f2"],license["f3"],license["f4"],license["f5"],license["f6"],license["f7"],license["f8"]
+    return {"result": True, "customers" : [customers]}
+
+
+def _getCustomer(email) :
+    PARAMS = {'search' : email, 'token' : customer_operations_token}
     URL = 'https://api.cryptolens.io/api/customer/GetCustomers'
     r = requests.get(url = URL, params = PARAMS)
     json_res = r.json()
@@ -29,24 +127,18 @@ def getCustomers(search_data) :
     
     
     # If customer is searched by email
-    if 'email' in search_data :
-        custs = []
-        for cust in customers :
-            if cust['email'] == search_data[search_key]:
-                custs.append(cust)
-        return custs
-    
-    
-    # If customer is searched by name
-    if 'name' in search_data :
-        custs = []
-        for cust in customers :
-            if cust['name'] == search_data[search_key]:
-                custs.append(cust)
-        return custs
-    
-    
-    return customers
+    custs = []
+    for cust in customers :
+        if cust['email'] == email:
+            res = _getCustomerLicenses(cust["id"])
+            if(res[0] == True) :
+                licenses = res[1]
+                if len(licenses) == 1 :
+                    license = licenses[0]
+                    cust["period"],cust["f1"],cust["f2"],cust["f3"],cust["f4"],cust["f5"],cust["f6"],cust["f7"],cust["f8"] = license["period"],license["f1"],license["f2"],license["f3"],license["f4"],license["f5"],license["f6"],license["f7"],license["f8"]
+            custs.append(cust)
+    return custs
+
 
 def createCustomers(csv_file, product_id) :
     customers = []
@@ -85,7 +177,7 @@ def createCustomers(csv_file, product_id) :
                                         f7=bool(int(cust['f7'])),
                                         f8=bool(int(cust['f8'])),
                                         customer_id=customer_id, 
-                                        max_no_of_machines=0)
+                                        max_no_of_machines=max_no_of_machines)
         
         # print(res_customer[0], create_key_res)
         print(f"f1 f2 f3 {cust['f1']} {cust['f2']} {cust['f3']}")
@@ -101,92 +193,70 @@ def createCustomers(csv_file, product_id) :
         a = 0
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def createCustomer(customerData):
+def _createCustomer(customerData):
     
     # Check if user exists with provided email
-    existing_customers = getCustomers({'email' : customerData["email"]})
+    existing_customers = _getCustomer(customerData["email"])
     if len(existing_customers) != 0:
-        print(f"User with mail {customerData['email']} already exists")
-        return False
+        return {"result" : False, "reason" : f"User with mail {customerData['email']} already exists"}
 
-    # Check if user exists with provided user
-    existing_customers = getCustomers({'name' : customerData["name"]})
-    if len(existing_customers) != 0:
-        print(f"User with name {customerData['name']} already exists")
-        return False
-    
     res_customer = Customer.add_customer(customer_operations_token, 
                       name = customerData["name"], 
                       email = customerData["email"], 
                       company_name = customerData["company"],
                       enable_customer_association = True)
-    print(f"res_customer {res_customer}")
     data = res_customer[0]
     if data:
-        return ({"customerId" : data['customerId'],  "secret" : data['secret']}, True)
+        create_key_res = Key.create_key(key_operations_token, 
+                                        customerData["productId"], 
+                                        period=customerData['period'], 
+                                        f1=bool(int(customerData['f1'])), 
+                                        f2=bool(int(customerData['f2'])), 
+                                        f3=bool(int(customerData['f3'])), 
+                                        f4=bool(int(customerData['f4'])),
+                                        f5=bool(int(customerData['f5'])),
+                                        f6=bool(int(customerData['f6'])),
+                                        f7=bool(int(customerData['f7'])),
+                                        f8=bool(int(customerData['f8'])),
+                                        customer_id=data['customerId'], 
+                                        max_no_of_machines=max_no_of_machines)
+        key_first_element = create_key_res[0]#
+        serial = key_first_element['key']#  ok
+        return ({"customerId" : data['customerId'],  "secret" : data['secret'], 'serial' : serial}, True)
+    
     else:
-        return ({}, False)
+        return ({"result" : False, "reason" : ""})
 
-
-def deleteCustomer(email):
+    
+def _deleteCustomer(email):
     
     # Get Customers with provided mail
-    customers = getCustomers({'email' : email})
+    customers = _getCustomer(email)
     if len(customers) != 1:
-        print(f"User with email {email} does not exist")
-        return False
+        return {"result" : False, "reason" : f"User with email {email} does not exist"}
     
     customer = customers[0]
     customerId = customer["id"]
     
+    
+    # Get licenses associated to customer
+    res = _getCustomerLicenses(customerId)
+    if(res[0] == True) :
+        licenseKeys = res[1]
+        if (len(licenseKeys) == 1) :
+            licenseKey = licenseKeys[0]
+            _revokeKey(licenseKey["productId"], licenseKey['key'])
+    
+    # Finaly delete customer
     PARAMS = {'customerId' : customerId, 'token' : customer_operations_token}
     URL = 'https://api.cryptolens.io/api/customer/RemoveCustomer'
     r = requests.get(url = URL, params = PARAMS)
     json_res = r.json()
     print(f"json_res is {json_res}")
-    a = 0
-    
-#Create Customer
-"""
-result = createCustomer({"name" : 'name', 
-                     "company" : 'company', 
-                     "email" : 'alekogureshidze@gmail.com',
-                     "period" : 30,	
-                     "f1" :  False,	
-                     "f2" :  False,	
-                     "f3" :  False,	
-                     "f4" :  False,	
-                     "f5" :  False,	
-                     "f6" :  False,	
-                     "f7" :  False,	
-                     "f8" :  False
-                     })
-"""
-
-
-def revokeKey(productId, key):
+    return json_res
+      
+        
+def _revokeKey(productId, key):
     PARAMS = {'productId' : productId, 
               'key' : key, 
               'customerId' : 0,  
@@ -196,17 +266,40 @@ def revokeKey(productId, key):
     json_res = r.json()
 
     res = Key.block_key(key_operations_token, productId, key)
-    a = 0
+    return {"result" : res[0]}
     
+def _revokeLicense(email) :
+    #Get user which corresponds to email
+    res = _getCustomer(email)
+    if len(res) != 1:
+        return {False, "Cannot find corresponding user"}
+    
+    customer = res[0]
+    customerId = customer['id']
+    
+    res = _getCustomerLicenses(customerId)
+    if(res[0] == True) :
+        licenseKeys = res[1]
+        if (len(licenseKeys) == 1) :
+            licenseKey = licenseKeys[0]
+            res = _revokeKey(licenseKey["productId"], licenseKey['key'])
+    
+    return res
 
-def createLicense(userEmail, license, product_id):
-    customers = getCustomers({"email": userEmail})
+def _createLicense(userEmail, license):
+    customers = _getCustomer(userEmail)
     if len(customers) != 1:
         print(f"User with email {userEmail} does not exist")
-        return False
-    customer_id = customers[0]['customerId']
+        return {"result" : False, "reason" : "There are more than 1 users associated with this email address"}
     
-    create_key_res = Key.create_key(key_operations_token, 
+    customer_id = customers[0]['id']
+    
+    res = _getCustomerLicenses(customer_id)
+    licenseKeys = res[1]
+    if (len(licenseKeys) != 0) :
+        return {"result" : False, "reason" : "User Already has Licens(e)s"}
+    else :
+        create_key_res = Key.create_key(key_operations_token, 
                                     product_id, 
                                     period=license['period'], 
                                     f1=bool(int(license['f1'])), 
@@ -218,22 +311,10 @@ def createLicense(userEmail, license, product_id):
                                     f7=bool(int(license['f7'])),
                                     f8=bool(int(license['f8'])),
                                     customer_id=customer_id, 
-                                    max_no_of_machines=0)
+                                    max_no_of_machines=max_no_of_machines)
+        key_first_element = create_key_res[0]#
+        serial = key_first_element['key'] #ok
+        return {"result" : True, "data" : {"email" : userEmail, 'serial' : serial}}
 
-
-revokeKey(15301, 'GJEMJ-HXAZD-KXXRO-ZYKRW')
-
-
-# Delete Customer  
-# deleteCustomer('name@gmail.com')  
-    
-    
-# Create Customers
-# createCustomers('customers.csv', 15301)
-
-# print(f"result is {result}")
-
-
-# customers = getCustomers({"email": "alekogureshidze@gmail.com"})
-# print(f"customers is {customers}")
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
--- readenv.py
+++ readenv.py
@@ -1,10 +1,11 @@
+import os
 # get CUSTOMER_OPERATIONS_TOKEN from environment
 if 'CUSTOMER_OPERATIONS_TOKEN' not in os.environ:
     print("No CUSTOMER_OPERATIONS_TOKEN variable present in environment")
     #exit(0)
 # this token must have addcustomer permission
 # customer_operations_token = os.environ['CUSTOMER_OPERATIONS_TOKEN']
-customer_operations_token = "WyIxOTAxNzg2MiIsIndBVW03ZE83bUZKSVFhQnp1eXQ3T0Nua2lBaDNxTkN5eWNxWEMyRVMiXQ=="
+customer_operations_token = "WyIxOTA3MDU1NSIsIk5BTkc3VUxpTitYcVJIU1ZhdTRiaTdYUGxnTFM3SVVGQ0QrbzkyUVgiXQ=="
 # customer_operations_token = ''
 
 
@@ -16,3 +17,33 @@ if 'KEY_OPERATIONS_TOKEN' not in os.environ:
 # key_operations_token = os.environ['KEY_OPERATIONS_TOKEN']
 key_operations_token = "WyIxOTA0MDY2OCIsIkI4TGd5U3FTdGJQZnpTT29aeWlyQjhxZFNzSDEzU3gyT3hsZVBqNlYiXQ=="
 # key_operations_token = ''
+
+
+
+# get COMPANY_NAME from environment
+if 'COMPANY_NAME' not in os.environ:
+    print("No COMPANY_NAME variable present in environment")
+    #exit(0)
+# this token must have addcustomer permission
+# company_name = os.environ['COMPANY_NAME']
+company_name = "company"
+# company_name = ''
+
+
+# get PRODUCT_ID from environment
+if 'PRODUCT_ID' not in os.environ:
+    print("No PRODUCT_ID variable present in environment")
+    #exit(0)
+# this token must have addcustomer permission
+# product_id = os.environ['PRODUCT_ID']
+product_id = 15301
+# product_id = ''
+
+# get MAX_NO_OF_MACHINES from environment
+if 'MAX_NO_OF_MACHINES' not in os.environ:
+    print("No MAX_NO_OF_MACHINES variable present in environment")
+    #exit(0)
+# this token must have addcustomer permission
+# product_id = os.environ['MAX_NO_OF_MACHINES']
+max_no_of_machines = 3
+# product_id = ''
